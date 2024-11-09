365b2e9b0f1319b7bc9b33dc9492f1b3f6eceb78
Masud Rana <91444955+masudprobd@users.noreply.github.com>
2024-11-09 16:44:52 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,24 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('pwa-cache').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/images/icons-192.png',
-          '/images/icons-512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  console.log('Service Worker installing...');
+  event.waitUntil(
+    caches.open('pwa-cache').then(cache => {
+      console.log('Caching files...');
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/images/icons-192.png',
+        '/images/icons-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
